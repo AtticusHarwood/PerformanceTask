4d1a58a4ed870c1480a9ec98e248fe60404a348d
AtticusHarwood <156236937+AtticusHarwood@users.noreply.github.com>
2024-01-10 13:02:14 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,99 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "pizza":return img`
+.............beebbbb............
+............eebbbb4bb...........
+............eb344bb4bb..........
+............e44334bb4bb.........
+............eb433344b4be........
+............4eb43344444be.......
+...........bd4eb43333344bb......
+..........b455d4443333444bb.....
+..........4d5555d444333444bb....
+.........4555555dd4b4443444be...
+........bd5555d555d4bb444444ee..
+........b55ddd665555bb4b44444ee.
+.......bd5555677655554ebb44444eb
+.......43222558855555d4eeb44b4ee
+......b422332ddd555222d4eebbb4be
+......be22232ed55522332db4ebbbbe
+.....bde22222e555e22232edd4bbbbe
+.....b52e222e3555e22222eddd4ebee
+....bd552eee355552e222e355544eee
+....665dd5555555552eee355dd4deee
+...6776555555555555555551554d4ee
+...4885222555dddd6655551544d4eee
+..b45522332555dd677611d444ddeee.
+..4d5222232e55555881d44ddd4eee..
+.bdd5e22222e555115114d54d4ee....
+.b55d2e222e351144d1d55eeee......
+bd5ddd2eee3d444555dd4e..........
+b555115dddd55d544eede...........
+4511d444d5544ee...4de...........
+41d4555d4ee........44...........
+41554eede.......................
+44ee...4e.......................
+`;
+            case "image2":
+            case "colors":return img`
+3 7 4 a e 
+`;
+            case "image3":
+            case "Burger":return img`
+fffffffffffccccc66666fffffffffff
+ffffffffccc4444444444666ffffffff
+ffffffcc444444444bb4444466ffffff
+fffffcb4444bb4444b5b444444bfffff
+ffffeb4444b5b44444b44444444bffff
+fffebb44444b4444444444b444446fff
+ffeb6bb444444444bb444b5b444446ff
+ffe6bb5b44444444b5b444b44bb44eff
+fe66b4b4444444444b4444444b5b44ef
+fe6bb444444444444444444444bb44ef
+eb66b44444bb444444444444444444be
+eb66bb444b5b44444444bb44444444be
+fb666b444bb444444444b5b4444444bf
+fcb666b44444444444444bb444444bcf
+ffbb6666b44444444444444444444bff
+fefbb66666bb4444444444444444bfef
+f86fcbb66666bbb44444444444bcc688
+8772effcbbbbbbbbbbbbbbbbcfc22778
+87722222cccccccccccccccc22226678
+f866622222222222222222222276686f
+fef866677766667777776667777fffef
+fbff877768f86777777666776fffffbf
+fbeffeefffeff7766688effeeeefeb6f
+f6bfffeffeeeeeeeeeeeeefeeeeebb6e
+f66ddfffffeeeffeffeeeeeffeedb46e
+fc66ddd4effffffeeeeeffff4ddb46ef
+ffc6b4dddddddddddddddddddb444eef
+ffff6bb444444444444444444444eeff
+ffffffbbbb4444444444444444eeffff
+ffffffffebbbbbb44444444eeeffffff
+ffffffffffffffffccccceefffffffff
+ffffffffffffffffffffffffffffffff
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
