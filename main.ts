4f2e052f0de1e6c73ecd28252da4d33b1b658cbb
Joaquin <156236096+Grandestella123@users.noreply.github.com>
2024-01-17 12:24:11 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,6 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    let mySprite: Sprite = null
+    mySprite.sayText(":)", 500, false)
+})
 let heart = sprites.create(assets.image`heart`, SpriteKind.Player)
 heart.setPosition(8, 9)
