21cd3cae975b77926ba541118b381b0a290b595f
Joaquin <156236096+Grandestella123@users.noreply.github.com>
2024-01-19 12:13:56 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,24 +5,7 @@ heart = sprites.create(assets.image`heart`, SpriteKind.Player)
 heart.setPosition(16, 9)
 heart = sprites.create(assets.image`heart`, SpriteKind.Player)
 heart.setPosition(24, 9)
-let player1 = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
+let player1 = sprites.create(assets.image`Burger`, SpriteKind.Player)
 tiles.placeOnTile(player1, tiles.getTileLocation(8, 8))
 scene.cameraFollowSprite(player1)
 controller.moveSprite(player1, 100, 100)
