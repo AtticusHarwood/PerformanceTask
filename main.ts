2ecc5cb380682bafbb4c4c49a8a7715f6dba4ac9
AtticusHarwood <156236937+AtticusHarwood@users.noreply.github.com>
2024-01-19 12:07:50 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let mySprite: Sprite = null
 tiles.setCurrentTilemap(tilemap`level`)
 let heart = sprites.create(assets.image`heart`, SpriteKind.Player)
 heart.setPosition(8, 9)
@@ -5,6 +6,7 @@ heart = sprites.create(assets.image`heart`, SpriteKind.Player)
 heart.setPosition(16, 9)
 heart = sprites.create(assets.image`heart`, SpriteKind.Player)
 heart.setPosition(24, 9)
-let player1 = sprites.create(assets.image`Burger`, SpriteKind.Player)
+let player1 = sprites.create(assets.image`myImage`, SpriteKind.Player)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(8, 8))
 scene.cameraFollowSprite(player1)
 controller.moveSprite(player1, 100, 100)
