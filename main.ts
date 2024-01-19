b4bcf74aa1cd06cf3f743b4d57bbf97480f0432e
AtticusHarwood <156236937+AtticusHarwood@users.noreply.github.com>
2024-01-19 12:17:07 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,3 @@
-let player1: Sprite = null
 tiles.setCurrentTilemap(tilemap`level`)
 let heart = sprites.create(assets.image`heart`, SpriteKind.Player)
 heart.setPosition(8, 9)
@@ -6,6 +5,24 @@ heart = sprites.create(assets.image`heart`, SpriteKind.Player)
 heart.setPosition(16, 9)
 heart = sprites.create(assets.image`heart`, SpriteKind.Player)
 heart.setPosition(24, 9)
+let player1 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . f f . . . . . . 
+    . . . . . . . . f . . . . . . . 
+    . . . . . . . . f . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . f f f f . f . . . . . . . 
+    . . . . . . . . f . . . . . . . 
+    . . . . . . . . f f . . . . . . 
+    . . . . . . . . . f f . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . f . . . . . . . . . . 
+    . . . . . . f f . . . . . . . . 
+    `, SpriteKind.Player)
 tiles.placeOnTile(player1, tiles.getTileLocation(8, 8))
 scene.cameraFollowSprite(player1)
 controller.moveSprite(player1, 100, 100)
